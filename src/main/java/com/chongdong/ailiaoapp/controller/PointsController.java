d564e19ed4249ecdc7dd9d25a750e39856924d55
ASUS <152153431@qq.com>
2023-09-22 12:06:02 +0800

    增加：评论、积分

--- src/main/java/com/chongdong/ailiaoapp/controller/PointsController.java
+++ src/main/java/com/chongdong/ailiaoapp/controller/PointsController.java
@@ -1,11 +1,16 @@
 package com.chongdong.ailiaoapp.controller;
 
+import com.chongdong.ailiaoapp.model.Points;
+import com.chongdong.ailiaoapp.model.ResponseMap;
 import com.chongdong.ailiaoapp.service.PointsService;
 import jakarta.annotation.Resource;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @Slf4j
 @CrossOrigin
@@ -14,4 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class PointsController {
     @Resource
     private PointsService pointsService;
+
+
+
 }
