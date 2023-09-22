cb8919d01b06179b8c954eb7306e29cf6db554af
minkai <951286607@qq.com>
2023-09-22 10:59:10 +0800

    测试

---
+++ src/main/java/com/chongdong/ailiaoapp/controller/TrendsController.java
@@ -0,0 +1,29 @@
+package com.chongdong.ailiaoapp.controller;
+
+
+import jakarta.annotation.Resource;
+import lombok.extern.log4j.Log4j;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+
+@RestController
+@RequestMapping("Trends")
+@Log4j
+public class TrendsController {
+
+
+
+
+
+
+}
